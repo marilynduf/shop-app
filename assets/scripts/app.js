19af9786c865a4a5e3a1fc8dbef8c6a146f5931a
M <marilynduf@gmail.com>
2025-11-05 15:50:58 -0500

    Add new class ProductList

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,40 +1,36 @@
 class Product {
-  // title = 'DEFAULT';
-  // imageUrl;
-  // description;
-  // price;
-
-  constructor(title, image, desc, price) {
-    this.title = title;
-    this.imageUrl = image;
-    this.description = desc;
-    this.price = price;
-  }
+    constructor(title, image, desc, price) {
+        this.title = title;
+        this.imageUrl = image;
+        this.description = desc;
+        this.price = price;
+    }
 }
 
-const productList = {
-  products: [
-    new Product(
-      'A Pillow',
-      'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
-      'A soft pillow!',
-      19.99
-    ),
-    new Product(
-      'A Carpet',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
-      'A carpet which you might like - or not.',
-      89.99
-    )
-  ],
-  render() {
-    const renderHook = document.getElementById('app');
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
-    for (const prod of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
+class ProductList {
+    products = [
+        new Product(
+            "A Pillow",
+            "https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
+            "A soft pillow!",
+            19.99
+        ),
+        new Product(
+            "A Carpet",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
+            "A carpet which you might like - or not.",
+            89.99
+        ),
+    ];
+    constructor() {}
+    render() {
+        const renderHook = document.getElementById("app");
+        const prodList = document.createElement("ul");
+        prodList.className = "product-list";
+        for (const prod of this.products) {
+            const prodEl = document.createElement("li");
+            prodEl.className = "product-item";
+            prodEl.innerHTML = `
         <div>
           <img src="${prod.imageUrl}" alt="${prod.title}" >
           <div class="product-item__content">
@@ -45,10 +41,11 @@ const productList = {
           </div>
         </div>
       `;
-      prodList.append(prodEl);
+            prodList.append(prodEl);
+        }
+        renderHook.append(prodList);
     }
-    renderHook.append(prodList);
-  }
-};
+}
 
+const productList = new ProductList();
 productList.render();
